cddd2f1659be3e43c0db810e1497a2e130eacf35
JoelNe <joel.nevala@gmail.com>
2021-10-06 15:16:30 +0300

    Login functionality.
    if you don't want to automatically login to your previous account, there is a Clear Prefs boolean in the Profile Handler Script. Change that to true to clear your saved login information.

--- Assets/Scripts/ProfileCreation/ProfileHandler.cs
+++ Assets/Scripts/ProfileCreation/ProfileHandler.cs
@@ -1,25 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using System.Linq;
+using UnityEngine.UI;
 using System;
 
 public class ProfileHandler : MonoBehaviour
 {
     public GameObject LogInScreen;
     public Profile userProfile;
-    private string username;
-    private string password;
-    private string passwordRepeat;
     public ProfileUIController profUIController;
-    private void Awake()
-    {
 
+    public InputField input_userName;
+    public InputField input_password;
+    public InputField input_passwordAgain;
+    public bool clearPrefs;
+
+    string userName;
+    string password;
+    string passwordAgain;
+
+    private void Start()
+    {
+        if (clearPrefs)
+        {
+            PlayerPrefs.DeleteKey(Profile.USERNAME_TAG);
+            PlayerPrefs.DeleteKey(Profile.PASSWORD_TAG);
+        }
+
+
+        if (PlayerPrefs.HasKey(Profile.USERNAME_TAG)) //Load if Prefs is founded
+        {
+            userName = PlayerPrefs.GetString(Profile.USERNAME_TAG);
+            password = PlayerPrefs.GetString(Profile.PASSWORD_TAG);
+            Client.Instance.BeginRequest_GetAllProfiles(OnGetProfilesForLoginRequestComplete);
+        }
     }
 
-    public void ChangeName(TextMeshProUGUI text)
+    public void OnClick_CreateNewUser()
     {
-        userProfile.displayName = text.text;
+        userName = input_userName.text;
+        password = input_password.text;
+        passwordAgain = input_passwordAgain.text;
+
+
+        if (password != passwordAgain)
+        {
+            Debug.Log("Passwords didn't match");
+            return;
+        }
+
+        Profile newProfile = new Profile(
+            1, 2,
+            userName, "No Name", password, "No Description",
+            3, 0, 0, Profile.ProfileType.Guest,
+            DateTime.Now
+        );
+
+        Client.Instance.BeginRequest_AddNewProfile(newProfile, OnCreateProfileRequestComplete);
+    }
+    void OnCreateProfileRequestComplete(string response)
+    {
+        if (response == "Success")
+            Debug.Log("New profile was created successfully");
+        else
+            Debug.Log("Failed to create profile: Response from server >> " + response);
+    }
+
+    public void OnClick_LogIn()
+    {
+        userName = input_userName.text;
+        password = input_password.text;
+        Client.Instance.BeginRequest_GetAllProfiles(OnGetProfilesForLoginRequestComplete);
+    }
+
+    void OnLogInRequestComplete(string response)
+    {
+        if (response == "Success")
+        {
+            Debug.Log("Log in was successful");
+            PlayerPrefs.SetString(Profile.USERNAME_TAG, userName);
+            PlayerPrefs.SetString(Profile.PASSWORD_TAG, password);
+            if (Client.Instance.profile_list.profiles != null && Client.Instance.profile_list.profiles.Length > 0)
+                FindObjectOfType<ProfileHandler>().userProfile = Client.Instance.profile_list.profiles.Where(x => x.userName == userName).First();
+            LogInScreen.SetActive(false);
+            profUIController.UpdateProfileCanvas();
+        }
+        else
+            Debug.Log("Log in Failed. Invalid username or password: Response from server >> " + response);
+    }
+    void OnGetProfilesForLoginRequestComplete(string response)
+    {
+        if (response[0] == '[')
+        {
+            Debug.Log("Getting Profiles List was Successful");
+            Client.Instance.BeginRequest_ValidatePassword(userName, password, OnLogInRequestComplete);
+        }
+        else
+        {
+            Debug.Log("Failed to get Profiles List. response: " + response);
+        }
+    }
+
+    public void ChangeName(string text)
+    {
+        userProfile.displayName = text;
     }
 
     public void ChangeAvatarID(int avatarID)
@@ -29,40 +114,11 @@ public class ProfileHandler : MonoBehaviour
 
     public void SaveProfileChanges()
     {
-        userProfile.SaveProfile();
+        
     }
 
     public Profile GetUserProfile()
     {
         return userProfile;
     }
-
-    public void UpdateUserName(TextMeshProUGUI text)
-    {
-        username = text.text;
-    }
-
-    public void UpdatePassword(TextMeshProUGUI text)
-    {
-        password = text.text;
-    }
-    public void UpdatePasswordRepeat(TextMeshProUGUI text)
-    {
-        passwordRepeat = text.text;
-    }
-
-
-
-    //public void Login()
-    //{
-    //    if(password != "")
-    //        userProfile = new Profile(username, Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(password)));
-    //}
-
-    //public void Register()
-    //{
-    //    profUIController.ChangeProfileInfo();
-    //    if (password == passwordRepeat && password != "")
-    //        userProfile = new Profile(username, password, true);
-    //}
 }
